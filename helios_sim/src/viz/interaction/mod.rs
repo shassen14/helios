7bbabd020cb4a3fd89928ac066f3a7698f5fdf7b
Samir Hassen <fake@email.com>
2026-07-27 00:53:55 -0500

    loading actions and mapping to keybindings implementations

--- helios_sim/src/viz/interaction/mod.rs
+++ helios_sim/src/viz/interaction/mod.rs
@@ -3,19 +3,28 @@ use crate::viz::interaction::actions::registry::ActionRegistry;
 use bevy::prelude::*;
 
 pub mod actions;
+pub mod keybindings;
 pub mod registration;
 
-// TODO: future Sampling, Keybinding set?
+// TODO: future Sampling
 #[derive(SystemSet, Clone, PartialEq, Eq, Hash, Debug)]
 pub enum InteractionSet {
     Registration,
+    KeyBinding,
 }
 
 pub struct ActionRegistryPlugin;
 
 impl Plugin for ActionRegistryPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<ActionRegistry>()
-            .configure_sets(Startup, InteractionSet::Registration);
+        app.init_resource::<ActionRegistry>().configure_sets(
+            Startup,
+            (InteractionSet::Registration, InteractionSet::KeyBinding).chain(),
+        );
+
+        app.add_systems(
+            Startup,
+            keybindings::load_keybindings.in_set(InteractionSet::KeyBinding),
+        );
     }
 }
